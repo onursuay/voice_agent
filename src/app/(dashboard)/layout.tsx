e70a38b924c647c1851b719d5bb42af70d897b93
onursuay <onursuay@hotmail.com>
2026-04-10 08:03:28 +0300

    auto: save changes

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -80,7 +80,7 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
       <CreditProvider>
         <div className="flex h-screen bg-gray-50">
           <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden">
+          <div className="min-w-0 flex-1 flex flex-col overflow-hidden">
             <Topbar />
             <main className="flex-1 overflow-y-auto p-6">{children}</main>
           </div>
