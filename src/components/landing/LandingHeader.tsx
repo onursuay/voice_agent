97bd6be03e75f4f73002637c25770df8ba9c14e3
onursuay <onursuay@hotmail.com>
2026-04-23 12:03:45 +0300

    auto: save changes

--- src/components/landing/LandingHeader.tsx
+++ src/components/landing/LandingHeader.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useRef, useEffect } from 'react'
 import Image from 'next/image'
-import Link from 'next/link'
+import { Link } from '@/i18n/navigation'
 import ScheduleModal from './ScheduleModal'
 
 interface Props {
