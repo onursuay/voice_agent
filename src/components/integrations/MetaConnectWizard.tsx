eec34c917041eb049235db51160c52790cb7ec72
onursuay <onursuay@hotmail.com>
2026-04-23 12:00:51 +0300

    auto: save changes

--- src/components/integrations/MetaConnectWizard.tsx
+++ src/components/integrations/MetaConnectWizard.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRouter } from '@/i18n/navigation';
 import { useTranslations } from 'next-intl';
 import { Check, RefreshCw, AlertTriangle } from 'lucide-react';
 
