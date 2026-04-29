c33bdf1edc14c4cbaa40a8a28249f974bc3799d3
onursuay <onursuay@hotmail.com>
2026-04-29 11:05:27 +0300

    Limit Google OAuth scope to drive.file

--- src/app/api/integrations/google/connect/route.ts
+++ src/app/api/integrations/google/connect/route.ts
@@ -39,10 +39,7 @@ export async function GET(request: NextRequest) {
   oauthUrl.searchParams.set('client_id', clientId);
   oauthUrl.searchParams.set('redirect_uri', redirectUri);
   oauthUrl.searchParams.set('response_type', 'code');
-  oauthUrl.searchParams.set('scope', [
-    'https://www.googleapis.com/auth/spreadsheets.readonly',
-    'https://www.googleapis.com/auth/drive.file',
-  ].join(' '));
+  oauthUrl.searchParams.set('scope', 'https://www.googleapis.com/auth/drive.file');
   oauthUrl.searchParams.set('access_type', 'offline');
   oauthUrl.searchParams.set('prompt', 'consent select_account');
   oauthUrl.searchParams.set('state', state);
